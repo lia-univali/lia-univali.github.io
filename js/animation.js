a453f1e5eaf0a49d71736083112c26a711e3f341
Cauê Ruan Kotarski <caue1017@hotmail.com>
2018-10-30 16:55:23 -0300

    :alembic: :lipstick: atualizando animações e começando responsividade

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@ $(function () {
     animacao = true;
     var $svg = $('svg').drawsvg({
         duration: 500,
-        stagger: 100
+        stagger: 500
     });
     $svg.drawsvg('animate');
 });
