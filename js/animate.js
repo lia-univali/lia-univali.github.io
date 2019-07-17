780c62115c1dd5565a86b1ac8058752f94247883
AlissonSteffens <ali.steffens@gmail.com>
2019-07-17 11:07:57 -0300

    :fire: removing old code

---
+++ js/animate.js
@@ -0,0 +1,14 @@
+VANTA.DOTS({
+  el: "#projetos",
+  color: 0xa7a7a7,
+  color2: 0x63a8be,
+  backgroundColor: 0xf8f9fa,
+  size: 6.30,
+  spacing: 58.00
+})
+VANTA.NET({
+  el: "#header",
+  color: 0x555555,
+  backgroundColor: 0x111111,
+  points: 8.00
+})
