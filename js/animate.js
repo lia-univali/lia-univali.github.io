290df6e0f0ea7fa95c47bf7b3ed6a2bceddd0cfe
AlissonSteffens <ali.steffens@gmail.com>
2019-07-22 10:29:05 -0300

    :art: new backgrounds

--- js/animate.js
+++ js/animate.js
@@ -1,11 +1,11 @@
-VANTA.DOTS({
-  el: "#projetos",
-  color: 0xa7a7a7,
-  color2: 0x63a8be,
-  backgroundColor: 0xf8f9fa,
-  size: 6.30,
-  spacing: 58.00
-})
+// VANTA.DOTS({
+//   el: "#projetos",
+//   color: 0xa7a7a7,
+//   color2: 0x63a8be,
+//   backgroundColor: 0xf8f9fa,
+//   size: 6.30,
+//   spacing: 58.00
+// })
 VANTA.NET({
   el: "#header",
   color: 0x555555,
