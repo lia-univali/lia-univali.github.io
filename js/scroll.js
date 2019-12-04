a48162e71882a3364874100ccaf93bd3379cc9f1
AlissonSteffens <ali.steffens@gmail.com>
2019-12-04 20:59:20 -0300

    :art: white icons

--- js/scroll.js
+++ js/scroll.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
             document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
+                behavior: 'smooth',
+                block: 'start'
             });
         });
     });
